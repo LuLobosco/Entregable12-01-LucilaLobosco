b8c479318b14541d9ce75dfb146064597aada69e
LuLobosco <lucilalobosco@gmail.com>
2023-01-28 01:26:49 -0300

    update_user

--- Escuela/Escuela/views.py
+++ Escuela/Escuela/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse 
 from django.shortcuts import render
 
---
+++ Escuela/users/forms.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User 
+from django import forms
+
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(max_length=100, required = True)
+    class Meta:
+        model = User  
+        fields = ['username','first_name','email','password1','password2']
+
+class UserUpdateForm(forms.ModelForm):
+    username = forms.CharField(max_length=100, required = True)
+    first_name = forms.CharField(max_length=100, required = True)
+    class Meta:
+        model = User  
+        fields = ['username','first_name']
